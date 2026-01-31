28e150b17a4685bba4bb1ac95b7ec7ea6cf1633c
abdulmalek qasem hatem <alhomedyabboud@gmail.com>
2026-01-31 03:30:04 +0300

    add lesson for day

--- test.js
+++ test.js
@@ -2100,3 +2100,62 @@ SubString : substring(Start [Required],End [Optional])
 //         behavior: "smooth"
 //     })
 // }
+
+// [==========================={EPsode[111]}==================]
+// window.localStorage.setItem("color","#F00");
+// window.localStorage.fontWeight = "bold";
+// window.localStorage['fontSize'] = '20px';
+
+
+// //get 
+// console.log(window.localStorage.getItem("color"));
+// console.log(window.localStorage.color);
+// console.log(window.localStorage["color"]);
+
+// //remove  one
+
+// // window.localStorage.removeItem("color");
+
+
+// // //Remove All 
+
+// // window.localStorage.clear();
+
+// // Get Key 
+// console.log(window.localStorage.key(0))
+// //set Color In Page 
+
+// document.body.style.backgroundColor =  window.localStorage.getItem("color");
+// console.log(window.localStorage);
+// console.log( typeof window.localStorage)
+// [==========================={EPsode[112]}==================]
+// let lis = document.querySelectorAll("ul li");
+// let exp = document.querySelector(".experiment");
+
+// // window.localStorage.clear()
+// if(window.localStorage.getItem("color")){
+//     // If There Color In LOcalStorage 
+//     // [1] Add Color To Div 
+//         exp.style.backgroundColor = window.localStorage.getItem("color");
+//     //    [2] Remove Active FRom class FRom All Lis 
+//        lis.forEach((li)=>{
+//             li.classList.remove("active");
+//         });
+//         // [3]Add Active Class To Current Color 
+//         document.querySelector(`[data-color="${window.localStorage.getItem("color")}"]`).classList.add("active");
+// }
+// lis.forEach((li) => {
+//     li.addEventListener("click", (e) => {
+//         // console.log(e.currentTarget.dataset.color);
+//         // Remove Active Class From all Lis 
+//         lis.forEach((li)=>{
+//             li.classList.remove("active");
+//         });
+//         // Add Active Class To Cureent Element 
+//         e.currentTarget.classList.add("active");
+//         // Add Current Clor To Local Storage 
+//         window.localStorage.setItem("color", e.currentTarget.dataset.color);
+//     //    Change Dive Background Color 
+//     exp.style.backgroundColor = e.currentTarget.dataset.color ;
+//     })
+// })
